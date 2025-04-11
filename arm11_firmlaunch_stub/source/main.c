800e41e7b482901b8e87c6ee9436fe45b044218c
ss2552 <85072753+ss2552@users.noreply.github.com>
2025-04-11 23:07:28 +0900

    Update main.c

--- arm11_firmlaunch_stub/source/main.c
+++ arm11_firmlaunch_stub/source/main.c
@@ -1,85 +1,148 @@
-#include "types.h"
+typedef unsigned int u32;
+typedef volatile u32 vu32;
+
+#define AUTO_EXIT_COUNT 999999
+
+#define IS3DS 1
+
+#if IS3DS
 
 #define LCD_REGS_BASE           0x10202000
 
+#define HID_PAD                (*(vu32 *)0x10146000 ^ 0xFFF)
+
 #define LCD_TOP_FILL_REG        *(vu32 *)(LCD_REGS_BASE + 0x200 + 4)
-#define LCD_BOTTOM_FILL_REG     *(vu32 *)(LCD_REGS_BASE + 0xA00 + 4)
+// #define LCD_BOTTOM_FILL_REG     *(vu32 *)(LCD_REGS_BASE + 0xA00 + 4)
 #define LCD_FILL_ENABLE         (1u << 24)
 
-#define SCREEN_TOP_WIDTH     400
-#define SCREEN_BOTTOM_WIDTH  320
-#define SCREEN_HEIGHT        240
-#define SCREEN_TOP_FBSIZE    (3 * SCREEN_TOP_WIDTH * SCREEN_HEIGHT)
-#define SCREEN_BOTTOM_FBSIZE (3 * SCREEN_BOTTOM_WIDTH * SCREEN_HEIGHT)
+#else
+#include <stdio.h>
+#define HID_PAD 0x00000000
+#endif
 
-struct fb {
-     u8 *top_left;
-     u8 *top_right;
-     u8 *bottom;
-} ;
+#define BUTTON_R1              (1 << 8)
+#define BUTTON_L1              (1 << 9)
+#define BUTTON_A               (1 << 0)
+#define BUTTON_B               (1 << 1)
+#define BUTTON_X               (1 << 10)
+#define BUTTON_Y               (1 << 11)
+#define BUTTON_SELECT          (1 << 2)
+#define BUTTON_START           (1 << 3)
+#define BUTTON_RIGHT           (1 << 4)
+#define BUTTON_LEFT            (1 << 5)
+#define BUTTON_UP              (1 << 6)
+#define BUTTON_DOWN            (1 << 7)
 
-static const struct fb fbs[2] =
-{
-    {
-        .top_left  = (u8 *)0x18300000,
-        .top_right = (u8 *)0x18300000,
-        .bottom    = (u8 *)0x18346500,
-    },
-    {
-        .top_left  = (u8 *)0x18400000,
-        .top_right = (u8 *)0x18400000,
-        .bottom    = (u8 *)0x18446500,
-    },
-};
+#define COLOR_BLACK   0x000000
+#define COLOR_RED     0x0000FF
+#define COLOR_GREEN   0x00FF00
+#define COLOR_YELLOW  0x00FFFF
+#define COLOR_BLUE    0xFF0000
+#define COLOR_MAGENTA 0xFF00FF
+#define COLOR_AQUA    0xFFFF00
+#define COLOR_WHITE   0xFFFFFF
 
-static void setupFramebuffers(const struct fb *fbs)
-{
-    *(vu32 *)0x10400468 = (u32)fbs[0].top_left;
-    *(vu32 *)0x1040046c = (u32)fbs[1].top_left;
-    *(vu32 *)0x10400494 = (u32)fbs[0].top_right;
-    *(vu32 *)0x10400498 = (u32)fbs[1].top_right;
-    *(vu32 *)0x10400568 = (u32)fbs[0].bottom;
-    *(vu32 *)0x1040056c = (u32)fbs[1].bottom;
+
+void fill_screen(u32 c){
+
+#if IS3DS
+    LCD_TOP_FILL_REG = LCD_FILL_ENABLE | c;
+#else
+    printf("%d\n", c);
+#endif
 }
 
-static void clearScreens(const struct fb *fb)
-{
-    //Setting up two simultaneous memory fills using the GPU
+void main(){
 
-    vu32 *REGs_PSC0 = (vu32 *)0x10400010,
-         *REGs_PSC1 = (vu32 *)0x10400020;
+    u32 n = 0;
 
-    REGs_PSC0[0] = (u32)fb->top_left >> 3; //Start address
-    REGs_PSC0[1] = (u32)(fb->top_left + SCREEN_TOP_FBSIZE) >> 3; //End address
-    REGs_PSC0[2] = 0; //Fill value
-    REGs_PSC0[3] = (2 << 8) | 1; //32-bit pattern; start
+    // fill black screen
+    fill_screen(COLOR_BLACK);
+    
+loop:
 
-    REGs_PSC1[0] = (u32)fb->bottom >> 3; //Start address
-    REGs_PSC1[1] = (u32)(fb->bottom + SCREEN_BOTTOM_FBSIZE) >> 3; //End address
-    REGs_PSC1[2] = 0; //Fill value
-    REGs_PSC1[3] = (2 << 8) | 1; //32-bit pattern; start
+    if(n > AUTO_EXIT_COUNT){
+        fill_screen(COLOR_WHITE);
+        while(n - 1000);
+        
+    }
+    
+#if !IS3DS
+    printf("i     : %d\nkey   : %i\n", n, HID_PAD);
+#endif
 
-    while(!((REGs_PSC0[3] & 2) && (REGs_PSC1[3] & 2)));
-}
+    switch(HID_PAD){
+        case BUTTON_A:
 
-void error(void)
-{
-    // Fill the screens with red
-    LCD_TOP_FILL_REG = LCD_FILL_ENABLE | 0xFF;
-    LCD_BOTTOM_FILL_REG = LCD_FILL_ENABLE | 0xFF;
-    while(true);
-}
+            if(n != 1){
+                n = 1;
+                
+                fill_screen(COLOR_RED);
+            }
+            
+            goto loop;
 
-void main(void)
-{
-    // Fill the screens with black
-    LCD_TOP_FILL_REG = LCD_FILL_ENABLE | 0;
-    LCD_BOTTOM_FILL_REG = LCD_FILL_ENABLE | 0;
+        case BUTTON_B:
+        
+            if(n != 2){
+                n = 2;
+                
+                fill_screen(COLOR_GREEN);
+            }
 
-    setupFramebuffers(fbs);
-    clearScreens(&fbs[0]);
-    clearScreens(&fbs[1]);
+            goto loop;
 
-    LCD_TOP_FILL_REG = 0;
-    LCD_BOTTOM_FILL_REG = 0;
+        case BUTTON_X:
+            
+            if(n != 3){
+                n = 3;
+                    
+                fill_screen(COLOR_YELLOW);
+            }
+
+            goto loop;
+
+        case BUTTON_Y:
+                
+            if(n != 4){
+                n = 4;
+                
+                fill_screen(COLOR_BLUE);
+            }
+
+            goto loop;
+
+        case BUTTON_LEFT:
+            
+            if(n != 5){
+                n = 5;
+                
+                fill_screen(COLOR_MAGENTA);
+            }
+
+            goto loop;
+
+        case BUTTON_RIGHT:
+            
+            if(n != 6){
+                n = 6;
+                
+                fill_screen(COLOR_AQUA);
+            }
+
+            goto loop;
+    
+        case 0x000000:
+
+            n++;
+
+            goto loop;
+
+        default:
+        
+            fill_screen(COLOR_WHITE);
+            
+            while(n++ > AUTO_EXIT_COUNT);
+            
+    }
 }
