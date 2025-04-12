807763b508edd8eb0541a072a729f8bb2cc28e2c
ss2552 <85072753+ss2552@users.noreply.github.com>
2025-04-12 14:28:49 +0900

    Update main.c

--- arm11_firmlaunch_stub/source/main.c
+++ arm11_firmlaunch_stub/source/main.c
@@ -1,8 +1,6 @@
 typedef unsigned int u32;
 typedef volatile u32 vu32;
 
-#define AUTO_EXIT_COUNT 999999
-
 #define LCD_REGS_BASE           0x10202000
 
 #define HID_PAD                (*(vu32 *)0x10146000 ^ 0xFFF)
@@ -11,18 +9,12 @@ typedef volatile u32 vu32;
 #define LCD_BOTTOM_FILL_REG     *(vu32 *)(LCD_REGS_BASE + 0xA00 + 4)
 #define LCD_FILL_ENABLE         (1u << 24)
 
-#define BUTTON_R1              (1 << 8)
-#define BUTTON_L1              (1 << 9)
 #define BUTTON_A               (1 << 0)
 #define BUTTON_B               (1 << 1)
 #define BUTTON_X               (1 << 10)
 #define BUTTON_Y               (1 << 11)
-#define BUTTON_SELECT          (1 << 2)
-#define BUTTON_START           (1 << 3)
-#define BUTTON_RIGHT           (1 << 4)
 #define BUTTON_LEFT            (1 << 5)
-#define BUTTON_UP              (1 << 6)
-#define BUTTON_DOWN            (1 << 7)
+#define BUTTON_RIGHT           (1 << 4)
 
 #define COLOR_BLACK   0x000000
 #define COLOR_RED     0x0000FF
@@ -34,35 +26,25 @@ typedef volatile u32 vu32;
 #define COLOR_WHITE   0xFFFFFF
 
 
-void fill_screen(u32 c){
-    
-    LCD_TOP_FILL_REG = LCD_FILL_ENABLE | c;
-    
-}
 
-void main(){
+int main(void){
 
     u32 n = 0;
 
-    // fill black screen
-    fill_screen(COLOR_BLACK);
+    LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_BLACK;
     LCD_BOTTOM_FILL_REG = LCD_FILL_ENABLE | COLOR_WHITE;
-    
-loop:
 
-    if(n > AUTO_EXIT_COUNT){
-        fill_screen(COLOR_WHITE);
-        while(n - 1000);
-        
-    }
+
+loop:
 
     switch(HID_PAD){
         case BUTTON_A:
 
             if(n != 1){
+
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_RED;
                 n = 1;
-                
-                fill_screen(COLOR_RED);
+
             }
             
             goto loop;
@@ -70,9 +52,10 @@ loop:
         case BUTTON_B:
         
             if(n != 2){
-                n = 2;
                 
-                fill_screen(COLOR_GREEN);
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_GREEN;
+                n = 2;
+
             }
 
             goto loop;
@@ -80,9 +63,10 @@ loop:
         case BUTTON_X:
             
             if(n != 3){
-                n = 3;
                     
-                fill_screen(COLOR_YELLOW);
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_YELLOW;
+                n = 3;
+
             }
 
             goto loop;
@@ -90,9 +74,10 @@ loop:
         case BUTTON_Y:
                 
             if(n != 4){
+
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_BLUE;
                 n = 4;
                 
-                fill_screen(COLOR_BLUE);
             }
 
             goto loop;
@@ -100,9 +85,10 @@ loop:
         case BUTTON_LEFT:
             
             if(n != 5){
+
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE |  COLOR_MAGENTA;
                 n = 5;
                 
-                fill_screen(COLOR_MAGENTA);
             }
 
             goto loop;
@@ -112,7 +98,7 @@ loop:
             if(n != 6){
                 n = 6;
                 
-                fill_screen(COLOR_AQUA);
+                LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_AQUA;
             }
 
             goto loop;
@@ -125,9 +111,8 @@ loop:
 
         default:
         
-            fill_screen(COLOR_WHITE);
+            LCD_TOP_FILL_REG = LCD_FILL_ENABLE | COLOR_WHITE;
             
-            while(n++ > AUTO_EXIT_COUNT);
-            
+            return;
     }
 }
